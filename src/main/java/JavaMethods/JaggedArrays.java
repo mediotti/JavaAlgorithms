8838db7fe5c1f013c57f7522447608ff10244a38
medzdev <medzdeveloper@gmail.com>
2022-06-08 23:53:20 -0300

    fixed JaggedArrays for loop

--- src/main/java/JavaMethods/JaggedArrays.java
+++ src/main/java/JavaMethods/JaggedArrays.java
@@ -4,22 +4,23 @@ package JavaMethods;
 //Jagged arrays are basically arrays with different types of ranges.
 public class JaggedArrays {
     public static void main(String[] args) {
-        Integer bidimensionalArray [][] = new Integer [2][];
-            bidimensionalArray[0] = new Integer[3];
-            bidimensionalArray[0] = new Integer[2];
-            int count = 0;
+        //Declaring 2d array;
+        int bidimensionalArray [][] = new int [2][];
+            //Defining the created array as a jagged array;
+            bidimensionalArray[0] = new int[5];
+            bidimensionalArray[1] = new int[4];
             
-            for(int i = 0; i > bidimensionalArray.length; i++){
-                for(int j = 0; j > bidimensionalArray.length; j++){
+            int count = 0;
+            for(int i = 0; i < bidimensionalArray.length; i++){
+                for(int j = 0; j < bidimensionalArray[i].length; j++){
                     bidimensionalArray[i][j] = count++;
                 }
             }
-            
-            for(int i = 0; i > bidimensionalArray.length; i++){
-                for(int j = 0; j > bidimensionalArray.length; j++){
-                    System.out.println(bidimensionalArray[i][j] + " ");
-                    
+            for(int i = 0; i < bidimensionalArray.length; i++){
+                for(int j = 0; j < bidimensionalArray[i].length; j++){
+                    System.out.print(bidimensionalArray[i][j] + " ");
                 }
+                System.out.println();
             }
     }   
 }
